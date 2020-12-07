5a036a1a2b58ca8b5952de1d1aacf0886fa647be
Ruipeng Han <ruipeng2@illinois.edu>
2020-12-07 00:39:55 -0600

    Add graph initializer that help do the work in the main.

--- tests/graphInitializationTest.cpp
+++ tests/graphInitializationTest.cpp
@@ -2,8 +2,21 @@
 // Created by Ruipeng Han on 12/5/20.
 //
 #include "../cs225/catch/catch.hpp"
+#include "/Users/ruipenghan/cs225git/final_project/graph.h"
+
+map<pair<string, string>, vector<string>> all_airlines_in_edge_map;
+/**
+ * This test file will test the initialization of the graph and reading of the file.
+ */
 using namespace std;
-TEST_CASE("test_get_anagrams", "[weight=15]")
+
+
+TEST_CASE("Sanity check", "part 0")
 {
-REQUIRE(1 == 1);
+    REQUIRE(1 == 1);
+}
+
+TEST_CASE("Sanity check", "part 1")
+{
+    REQUIRE(1 == 1);
 }
