95f76ccc6b6b0f7f5cb2747e49413c0428914634
Ruipeng Han <ruipeng2@illinois.edu>
2020-12-10 07:58:00 -0600

    Add a basic user-interface engine.

---
+++ UserInterface.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Ruipeng Han on 12/10/20.
+//
+
+#include "UserInterface.h"
+UserInterface::UserInterface() {
+    end_ = false;
+    type_ = -1;
+}
+
+void UserInterface::Communicate() {
+
+    while (!end) {
+
+    }
+}
